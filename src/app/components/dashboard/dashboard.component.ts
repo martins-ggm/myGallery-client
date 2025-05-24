ad219935d8abcc9a42f73a748444e5777f663f44
martins-ggm <guilherme.ggm@outlook.com>
2025-05-24 19:14:22 -0300

    initial dashboard update

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NgOptimizedImage],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
